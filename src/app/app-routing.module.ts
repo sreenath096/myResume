71b66c6740e868c1051e304ce416717013ad5db6
Sreenath Angamuthu <sreenath096@gmail.com>
2022-08-10 10:43:03 +0530

    Add Home Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExperienceDetailsComponent } from './experience-details/experience-details.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: 'experiencedetails', component: ExperienceDetailsComponent },
+      // { path: 'achievementdetails', component: AchievementDetailsComponent },
+      // { path: 'certificationdetails', component: CertificationDetailsComponent },
+      // { path: 'educationdetails', component: EducationDetailsComponent },
+      // { path: 'skilldetails', component: SkillDetailsComponent },
+      // { path: 'downloadresume', component: DownloadResumeComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
